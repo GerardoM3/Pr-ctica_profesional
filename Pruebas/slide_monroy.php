b23651a7d00f1cb97254016bc416f47be94346b6
GerardoM3 <gerardomonroy13@gmail.com>
2022-11-03 21:36:53 -0600

    Create slide_monroy.php

---
+++ Pruebas/slide_monroy.php
@@ -0,0 +1,142 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title class="title-document">Slide</title>
+
+
+	<style type="text/css">
+		.content-main{
+			margin: 5em;
+		}
+
+		.card-1, .card-2,.card-3, .card-4, .card-5, .card-6, .card-7, .card-8{
+			display: inline-block;
+			width: 500px;
+			padding: 1rem;
+			margin: 1rem;
+			border: solid black;
+			border-width: 3px;
+			border-radius: 25px;
+		}
+
+		
+
+
+	</style>
+
+
+</head>
+<body>
+	<div class="content-main">
+		<div class="card-content">
+			<div class="card-1">
+				<div class="title-card">
+					<h2 class="h2-title">Tarjeta 1</h2>
+				</div>
+				<div class="content-card">
+					<p class="p">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+					consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+				</div>
+			</div>
+			<div class="card-2">
+				<div class="title-card">
+					<h2 class="h2-title">Tarjeta 2</h2>
+				</div>
+				<div class="content-card">
+					<p class="p">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+					consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+				</div>
+			</div>
+			<div class="card-3">
+				<div class="title-card">
+					<h2 class="h2-title">Tarjeta 3</h2>
+				</div>
+				<div class="content-card">
+					<p class="p">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+					consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+				</div>
+			</div>
+			<div class="card-4">
+				<div class="title-card">
+					<h2 class="h2-title">Tarjeta 4</h2>
+				</div>
+				<div class="content-card">
+					<p class="p">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+					consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+				</div>
+			</div>
+			<div class="card-5">
+				<div class="title-card">
+					<h2 class="h2-title">Tarjeta 5</h2>
+				</div>
+				<div class="content-card">
+					<p class="p">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+					consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+				</div>
+			</div>
+
+			<div class="card-6">
+				<div class="title-card">
+					<h2 class="h2-title">Tarjeta 6</h2>
+				</div>
+				<div class="content-card">
+					<p class="p">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+					consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+				</div>
+			</div>
+			<div class="card-7">
+				<div class="title-card">
+					<h2 class="h2-title">Tarjeta 7</h2>
+				</div>
+				<div class="content-card">
+					<p class="p">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+					consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+				</div>
+			</div>
+			<div class="card-8">
+				<div class="title-card">
+					<h2 class="h2-title">Tarjeta 8</h2>
+				</div>
+				<div class="content-card">
+					<p class="p">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+					consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
