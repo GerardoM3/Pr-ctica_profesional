2559e12833cd481026a6be1c4b5b94782b68d376
GerardoM3 <gerardomonroy13@gmail.com>
2022-12-08 15:52:39 -0600

    MVC de servicios de alcaldía añadido

---
+++ form/mvc_servicios_alcaldia/Controller/servicio.controller.php
@@ -0,0 +1,64 @@
+<?php
+require_once 'Model/servicio_alcaldia.php';
+
+class ServicioController{
+
+	private $model;
+	
+	function __CONSTRUCT()
+	{
+		$this->model = new Servicio_Alcaldia();
+	}
+
+	public function Index_Servicios(){
+		require_once 'View/haeder.php';
+		require_once 'View/';
+		require_once 'View/header.php';
+	}
+
+	public function Crud_Servicio(){
+		$alm = new Servicio_Alcaldia();
+
+		if (isset($_REQUEST['id_cods'])) {
+			$alm = $this->model->getting($_REQUEST['id_cods']);
+		}
+
+		require_once 'View/header.php';
+		require_once 'View/';
+		require_once 'View/footer.php';
+	}
+
+	public function Guardar_Servicio(){
+		$alm = new Servicio_Alcaldia();
+
+		$alm->cod1 = $_REQUEST['cod1'];
+		$alm->cod2 = $_REQUEST['cod2'];
+		$alm->cod3 = $_REQUEST['cod3'];
+		$alm->cod4 = $_REQUEST['cod4'];
+		$alm->descripcion_servicio = $_REQUEST['descripcion_servicio'];
+		$alm->descripcion_servicio_abreviado = $_REQUEST['descripcion_servicio_abreviado'];
+		$alm->unidad_medida = $_REQUEST['unidad_medida'];
+		$alm->tarifa_actual = $_REQUEST['tarifa_actual'];
+		$alm->tarifa_anterior = $_REQUEST['tarifa_anterior'];
+		$alm->periodo_vigencia_tarifa = $_REQUEST['periodo_vigencia_tarifa'];
+		$alm->tipo_concepto = $_REQUEST['tipo_concepto'];
+		$alm->tipo_cobro = $_REQUEST['tipo_cobro'];
+
+		$alm->cod1 > 0 AND cod2 > 0 AND cod3 > 0 AND cod4 > 0 ? $this->model->Actualizar($alm) : $this->model->Registrar($alm);
+
+		/*if(($alm->cod1 > 0) AND ($alm->cod2 > 0) AND ($alm->cod3 > 0) AND ($alm->cod4 > 0)){
+			$this->model->Actualizar($alm);
+		}else{
+			$this->model->Registrar($alm);
+		}*/
+
+		header('Location: index.php');
+
+	}
+
+	public function Eliminar(){
+		$this->model->Eliminar($_REQUEST['cod1'], $_REQUEST['cod2'], $_REQUEST['cod3'], $_REQUEST['cod4']);
+		header('Location: index.php');
+	}
+}
+?>
